8374050af7935b1323e26575ce751b4766b5a993
Avtandil <avtandilqazxswedc@gmail.com>
2023-05-02 16:45:07 +0300

    simple popup html&scss

---
+++ src/widgets/assets/index.ts
@@ -0,0 +1,6 @@
+import gmailLogo from './Gamil logo.svg'
+import microsoftLogo from './Microsoft logo.svg'
+import googleLogo from './Google logo.svg'
+
+
+export {gmailLogo, microsoftLogo, googleLogo}
--- src/widgets/login-popup/index.tsx
+++ src/widgets/login-popup/index.tsx
@@ -1,15 +1,21 @@
-import React, { HTMLAttributes } from 'react';
+import React, { FC, useState } from 'react';
 import styles from './styles.module.scss'
 import { CloseBtn } from './../../shared/ui';
-
+import {gmailLogo, microsoftLogo, googleLogo} from '../assets/index'
 interface popupProps {
   closePopup: React.MouseEventHandler<HTMLButtonElement>
 }
 
+export const loginPopup: FC<popupProps> = ({ closePopup }) => {
 
-export const loginPopup = ({ closePopup }: popupProps) => {
+  const types = {
+    emailLogin: 'emailLogin',
+    emailRegistration: 'emailRegistration',
+    google: 'google',
+    microsoft: 'microsoft'
+  }
 
-  
+  const [social, setSocial] = useState(types.emailLogin)
 
   return (
     <div className={styles.loginPopup} >
@@ -20,23 +26,52 @@ export const loginPopup = ({ closePopup }: popupProps) => {
       <div className={styles.content}>
         <h3 className={styles.content_title}>Let's get you signed in</h3>
         <div className={styles.socials}>
-          <h5 className={styles.socials_title}>Use this account</h5>
-          <div className={styles.social} onClick={e=>console.log(e)}>
-            <img src="/src/widgets/assets/Gamil logo.svg" alt="microsoftLogo.svg" />
+          <h5 className={styles.socials_title}>Use this way</h5>
+          <div
+            className={styles.social+ ' ' + (social === types.emailLogin && styles.active)}
+            onClick={() => setSocial(types.emailLogin)}
+          >
+            <img src={gmailLogo} alt="GamilLogo.svg" />
             <div className={styles.txtWrapper} >
-              <p className={styles.social_title}>Sign in with email</p>
+              <p className={styles.social_title}>Login with email</p>
               <p className={styles.social_type}>Email</p>
             </div>
           </div>
-          <div className={`${styles.social} ${styles.active }`}>
-            <img src="/src/widgets/assets/Gamil logo.svg" alt="microsoftLogo.svg" />
-            <div className={styles.txtWrapper}>
-              <p className={styles.social_title}>Sign in with email</p>
+          <div
+            className={styles.social+ ' ' + (social === types.emailRegistration && styles.active)}
+            onClick={() => setSocial(types.emailRegistration)}
+          >
+            <img src={gmailLogo} alt="GamilLogo.svg" />
+            <div className={styles.txtWrapper} >
+              <p className={styles.social_title}>Create account with email</p>
               <p className={styles.social_type}>Email</p>
+            </div>
+          </div>
+          
+          <h5 className={styles.socials_title}>Use different way to sign in</h5>
+          <div
+            className={styles.social+ ' ' + (social === types.microsoft && styles.active)}
+            onClick={() => setSocial(types.microsoft)}
+          >
+            <img src={microsoftLogo} alt="microsoftLogo.svg" />
+            <div className={styles.txtWrapper} >
+              <p className={styles.social_title}>Microsoft account</p>
+              <p className={styles.social_type}>Email, phone, or Skype</p>
+            </div>
+          </div>
+          <div
+            className={styles.social+ ' ' + (social === types.google && styles.active)}
+            onClick={() => setSocial(types.google)}
+          >
+            <img src={googleLogo} alt="googleLogo.svg" />
+            <div className={styles.txtWrapper} >
+              <p className={styles.social_title}>Google account</p>
+              <p className={styles.social_type}>Email, phone</p>
             </div>
           </div>
         </div>
       </div>
+      <button className={styles.continueBtn}>Continue</button>
     </div>
   )
 }
