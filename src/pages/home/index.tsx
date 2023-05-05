6d59d90ef21f97ad12a1eb0d084e38e17d816aac
Avtandil <avtandilqazxswedc@gmail.com>
2023-05-05 13:44:56 +0300

    add counter

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,15 @@
+import { useAppDispatch, useAppSelector } from "../../app/store/store";
 import styles from "./styles.module.scss";
 import { Link } from "react-router-dom";
+import { increment } from "../../features/counter/counterSlice";
 
 
 const homePage = () => {
+  const dispatch = useAppDispatch()
+  const count = useAppSelector((state) => state.counter.value)
   return (
     <div className={styles.root}>
+      <button onClick={() => dispatch(increment())}>{count}</button>
       <div className={styles.content}>
         <h1>this is home page</h1>
         <Link to='/auth' ><button>to auth</button></Link>
