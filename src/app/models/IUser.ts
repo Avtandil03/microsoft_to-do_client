9ff067e80d7e48b2a9676d15be5421917c5db3b3
Avtandil <avtandilqazxswedc@gmail.com>
2023-05-05 11:39:11 +0300

    reset main , clean from auth logic

--- src/app/models/IUser.ts
+++
@@ -1,6 +0,0 @@
-export interface IUser{
-  id_users?: number
-  name: string
-  email: string
-  isActivated: boolean
-}
--- src/app/models/response/AuthResponse.ts
+++
@@ -1,7 +0,0 @@
-import { IUser } from "../IUser"
-
-export interface AuthResponse {
-  accesToken: string
-  refreshToken: string
-  user: IUser
-}
