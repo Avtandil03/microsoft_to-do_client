617d8df2264ee9ad5905f3aa4ecf17c11e4a32f4
Avtandil <avtandilqazxswedc@gmail.com>
2023-05-11 21:03:10 +0300

    logout

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,11 +1,25 @@
+import { useAppDispatch } from "../../app/store/store";
+import { useLogoutUserMutation } from "../../features/api/apiSlice";
+import { resetUserData, setIsAuth } from "../../features/user/userSlice";
 import styles from "./styles.module.scss";
 import { Link } from "react-router-dom";
 
 
 const settingsPage = () => {
+
+  const [logoutUser] = useLogoutUserMutation()
+  const dispatch = useAppDispatch()
+
+  const handleClick = () => {
+    logoutUser()
+    localStorage.removeItem('token')
+    dispatch(setIsAuth(false))
+    dispatch(resetUserData())
+  }
   return (
     <div className={styles.root}>
       <div className={styles.content}>
+        <button onClick={handleClick}>logout</button>
         <h1>this is settings page</h1>
         <Link to='/'><button>to home</button></Link>
       </div>
