59f5d87d1431e5fd685b41a843bc0dd4316c3483
Avtandil <avtandilqazxswedc@gmail.com>
2023-05-12 15:40:01 +0300

    change Provider location

--- src/app/providers/with-router.tsx
+++ src/app/providers/with-router.tsx
@@ -1,14 +1,10 @@
 import { Suspense } from "react"
 import { BrowserRouter } from "react-router-dom"
-import { store } from "../store/store"
-import { Provider } from "react-redux"
 
 export const withRouter = (component: () => React.ReactNode) => () => (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Suspense fallback="Loading...">
-        {component()}
-      </Suspense>
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter>
+    <Suspense fallback="Loading...">
+      {component()}
+    </Suspense>
+  </BrowserRouter>
 )
